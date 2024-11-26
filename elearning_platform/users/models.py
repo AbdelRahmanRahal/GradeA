e9949e57cbb28fd193372b6501a11c989d3dea3b
Mahmoud Mohamed <M.mohamed2213@nu.edu.eg>
2024-11-26 23:19:09 +0200

    Use Django's custom user model to extend or modify the default User model to accommodate roles (Student, Instructor, Admin).

--- elearning_platform/users/models.py
+++ elearning_platform/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('instructor', 'Instructor'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return self.username
+
