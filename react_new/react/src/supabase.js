a0f2e1c1d2c4c8f5101d01f0fbaab4e6cc827e1e
Mahmoud mohamed <143263824+Mahmoudmohamed125@users.noreply.github.com>
2024-12-17 22:37:08 +0200

    Delete react_new/react directory

--- react_new/react/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://omdpgnhflouzknikcpky.supabase.co';
-// Finally filling in the key safely after configuring the RLS :p
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZHBnbmhmbG91emtuaWtjcGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4OTQ4MTAsImV4cCI6MjA0ODQ3MDgxMH0.ZO6m8tZC5ewqLZqqpFfqjxMxm-Tz7IPPx9AqDtJsVAI';
-export const supabase = createClient(supabaseUrl, supabaseKey);
